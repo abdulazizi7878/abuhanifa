7f279793d6b26fab5e6c5540095f1fae31ce34f2
abdulazizi7878 <abdulaizizi707@gmail.com>
2026-08-15 18:35:49 +0300

    Vercel analytics library installed

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import "../globals.css";
 import {NextIntlClientProvider} from 'next-intl';
 import {getMessages} from 'next-intl/server';
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
